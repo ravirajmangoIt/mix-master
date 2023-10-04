ee7e819db4997df137aa00c0e834b947fa9bfe9b
raviraj <raviraj.mangoIt@gmail.com>
2023-10-04 11:53:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Landing,
+  NewsLetter,
+  SinglePageError,
+} from "./pages";
+
+import { loader as landingLoader } from "./pages/landing";
+import { loader as singleCocktailLoader } from "./pages/cocktail";
+import { action as newsletterAction } from "./pages/newsLetter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <SinglePageError />,
+        loader: landingLoader(queryClient),
+      },
+      {
+        path: "cocktail/:id",
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader(queryClient),
+        element: <Cocktail />,
+      },
+      {
+        path: "newsletter",
+        element: <NewsLetter />,
+        action: newsletterAction,
+        errorElement: <SinglePageError />,
+      },
+      {
+        path: "about",
+        element: <About />,
+        children: [
+          {
+            index: true,
+            element: <h2>our company</h2>,
+          },
+          {
+            path: "person",
+            element: <h2>john doe</h2>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+export { default as Landing } from "./landing";
+export { default as About } from "./about";
+export { default as Cocktail } from "./cocktail";
+export { default as Error } from "./error";
+export { default as HomeLayout } from "./homeLayout";
+export { default as NewsLetter } from "./newsLetter";
+export { default as SinglePageError } from "./singlePageError";
+
+
+
+
